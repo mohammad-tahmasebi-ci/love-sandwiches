8a7343b3d1357f200d64d6ced5c3445b15cd21c5
Mohammad Tahmasebi <mohammad.tahmasebi@students.codeinstitute.net>
2024-12-05 19:29:48 +0000

    Update worksheet with a row of data

--- run.py
+++ run.py
@@ -20,13 +20,21 @@ SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
 
 def get_sales_data():
-    print('Please enter sales data from the last market')
-    print('Data should be six numbers, separated by commas')
-    print('Example: 10,20,30,40,50,60')
 
-    data_str = input('Enter your data here: ')
-    sales_data = data_str.split(',')
-    validate_data(sales_data)
+    """
+    Get sales data from the user
+    """
+
+    while True:
+        print('Please enter sales data from the last market')
+        print('Data should be six numbers, separated by commas')
+        print('Example: 10,20,30,40,50,60')
+
+        data_str = input('Enter your data here: ')
+        sales_data = data_str.split(',')
+        if validate_data(sales_data):
+            break
+        
 
 def validate_data(values):
 
@@ -37,12 +45,27 @@ def validate_data(values):
     """
 
     try:
-        values = [int(val) for val in values]
+        [int(val) for val in values]
         if len(values) != 6:
             raise ValueError(f'Exactly 6 values is required, you provided {len(values)}')
     except ValueError as e:
         print(f'Invalid data: {e}, please try again.\n')
+        return False
+    
+    return True
         
-    print(values)
+def update_sales_worksheet(data):
+    
+    """
+    update sales worksheet, add new row with the list data provided
+    """
 
-get_sales_data()
+    print('updating sales worksheet...\n')
+    sales_worksheet = SHEET.worksheet('sales')
+    sales_worksheet.append_row(data)
+    print('sales worksheet updated successfully\n')
+
+
+data = get_sales_data()
+sales_data = [int(val) for val in data]
+update_sales_worksheet(sales_data)
